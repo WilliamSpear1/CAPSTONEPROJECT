1893232cc50eb69147078385a7dfc592f630b0d9
BSteadman321 <bsteadman321@gmail.com>
2021-10-09 21:49:46 -0400

    Overhauled entire game foundation
    Basically ended up starting from scratch and had an extended group meetup where everybody contributed to this large push. All group members worked equally on this push.
    
    Co-Authored-By: Andr3wx <89699043+Andr3wx@users.noreply.github.com>
    Co-Authored-By: WilliamSpear1 <78100370+WilliamSpear1@users.noreply.github.com>
    Co-Authored-By: wdharter <91571593+wdharter@users.noreply.github.com>

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public GameObject player;
-    public float offsetSmoothing;
-    private Vector3 playerPosition;
+    //public GameObject player;
+    //public float offsetSmoothing;
+    //private Vector3 playerPosition;
+    Transform CamTransform;
+    public Transform Player;
+    float followspeed = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        CamTransform = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
+        Vector3 targetPosition = new Vector3(Player.position.x, CamTransform.position.y, CamTransform.position.z);
+        CamTransform.position = Vector3.Lerp(CamTransform.position, targetPosition, Time.deltaTime * followspeed);
+
+        //playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        //transform.position = Vector3.Lerp(transform.position, playerPosition, offsetSmoothing * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Megabot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Megabot : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    float dirX, moveSpeed = 1.5f;
+    bool facingRight = true;
+    Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+            rb.AddForce(Vector2.up * 200f);
+
+        SetAnimationState();
+
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);    
+    }
+
+    void LateUpdate()
+    {
+        CheckWhereToFace();    
+    }
+
+    void SetAnimationState()
+    {
+        if(dirX == 0)
+            anim.SetBool("isWalking", false);
+
+        if(rb.velocity.y == 0)
+            anim.SetBool("isJumping", false);
+
+        if (Mathf.Abs(dirX) == 1.5 && rb.velocity.y == 0)
+            anim.SetBool("isWalking", true);
+
+        if (rb.velocity.y > 0)
+            anim.SetBool("isJumping", true);
+    }
+
+    void CheckWhereToFace()
+    {
+        if (dirX > 0)
+            facingRight = true;
+        else if (dirX < 0)
+            facingRight = false;
+
+        if(((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
+}
