e407ebcfb55a1f9ff590f8d77252a817542514b3
WilliamSpear1 <78100370+WilliamSpear1@users.noreply.github.com>
2021-10-10 18:07:51 -0400

    Blue Enemy
    Went ahead and wrote the scripts for the flying enemy now it chases the player and if the player touches or shoots it the enemy will explode upon impact

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
+    private int damage = 50;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,10 @@ public class Bullet : MonoBehaviour
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
         Debug.Log(hitInfo.name);        //This will output in log what the bullet collides with (if anything)
+       BlueFlyingEnemy enemy = hitInfo.GetComponent<BlueFlyingEnemy>();
+        if (enemy != null) {
+            enemy.TakeDamage(damage);
+        }
         Destroy(gameObject);
     }
 }
